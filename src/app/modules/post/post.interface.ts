8e7e7dd723a23fc3a3e35f553778a674bd177e7a
Nabiur siddique <nabiursiddique01@gmail.com>
2024-10-02 15:59:30 +0600

    Post route, interface and model is created

---
+++ src/app/modules/post/post.interface.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+export interface TPost {
+  _id: string;
+  title: string;
+  category: 'tips' | 'story';
+  isPremium: boolean;
+  content: string;
+  image: string;
+  upVotes: number;
+  downVotes: number;
+  comments: Comment[];
+  isPublished: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+  voters: {
+    userId: mongoose.Types.ObjectId;
+    voteType: 'up' | 'down';
+  }[];
+}
+
+export interface Comment {
+  _id?: string;
+  userId: mongoose.Types.ObjectId;
+  content: string;
+  createdAt?: Date;
+}
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { UserRoutes } from '../modules/user/user.route';
 import { AuthRoutes } from '../modules/auth/auth.route';
+import { PostRoutes } from '../modules/post/post.route';
 
 const router = Router();
 
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: '/auth',
     route: AuthRoutes,
+  },
+  {
+    path: '/post',
+    route: PostRoutes,
   },
 ];
 
